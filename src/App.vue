610d0981e2a762bbf03bebe8478dca0f8a284024
xiaoli <2585960098@qq.com>
2023-07-03 01:20:35 +0800

    22

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>基本模板！
+    <router-link to="/home">home</router-link>
+    <router-link to="/order">order</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app{
+  width: 100%;
+  height: 100%;
+}
+</style>
