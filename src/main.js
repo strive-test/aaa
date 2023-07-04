cad3b46d918201554f6f6628b5e87fa25694a55b
xiaoli <2585960098@qq.com>
2023-07-04 22:02:32 +0800

    test:测试一下

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '@/assets/reset.css'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@/assets/reset.css";
+import router from "./router";
+console.log("dev/2.0.1");
+createApp(App).use(router).mount("#app");
