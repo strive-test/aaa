cc2ddf1fbc9e468d623b74945903d05a38ef603b
xiaoli <2585960098@qq.com>
2023-07-07 19:41:41 +0800

    测试bug

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "@/assets/reset.css";
 import router from "./router";
-console.log("dev/2.0.1");
+console.log("dev/2.0.2");
 createApp(App).use(router).mount("#app");
